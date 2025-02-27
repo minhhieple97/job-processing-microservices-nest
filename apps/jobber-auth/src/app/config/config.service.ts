f13acc4a148147ca5de9b1f287df081140635909
hiep-le_fmgl <hiep.le@fortescue.com>
2025-02-27 16:28:29 +0700

    feat: Setup config module

---
+++ apps/jobber-auth/src/app/config/config.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService as NestConfigService } from '@nestjs/config';
+
+@Injectable()
+export class ConfigService {
+  constructor(private configService: NestConfigService) {}
+
+  get databaseUrl(): string {
+    return this.configService.get<string>('AUTH_DATABASE_URL');
+  }
+
+  get pulsarUrl(): string {
+    return this.configService.get<string>('PULSAR_URL');
+  }
+
+  get pulsarAdminUrl(): string {
+    return this.configService.get<string>('PULSAR_ADMIN_URL');
+  }
+
+  get pulsarTenant(): string {
+    return this.configService.get<string>('PULSAR_TENANT');
+  }
+
+  get pulsarNamespace(): string {
+    return this.configService.get<string>('PULSAR_NAMESPACE');
+  }
+
+  get nodeEnv(): string {
+    return this.configService.get<string>('NODE_ENV');
+  }
+
+  get isProduction(): boolean {
+    return this.nodeEnv === 'production';
+  }
+}
