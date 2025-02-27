b3afb1dee36b19d9a3a66b3349febd0a7626bb5d
hiep-le_fmgl <hiep.le@fortescue.com>
2025-02-27 13:30:36 +0700

    Setup graphql

--- apps/jobber-auth/src/app/app.module.ts
+++ apps/jobber-auth/src/app/app.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ApolloDriver } from '@nestjs/apollo';
+import { ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      useGlobalPrefix: true,
+      playground: {
+        settings: {
+          'request.credentials': 'include',
+        },
+      },
+      context: ({ req, res }) => ({ req, res }),
+      autoSchemaFile: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
