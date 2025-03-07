6c9cfdd687f852252ffd48bafb01743f11f70789
hiep-le_fmgl <ext.hiep.le@softwareone.com>
2025-03-07 11:20:17 +0700

    feat: Setup connect with grpc server

--- apps/jobber-auth/src/main.ts
+++ apps/jobber-auth/src/main.ts
@@ -3,14 +3,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ConfigService } from './app/config/config.service';
 import cookieParser from 'cookie-parser';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+import { protobufPackage } from 'proto-types/proto/auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const globalPrefix = 'api';
 
-  app.setGlobalPrefix(globalPrefix);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: protobufPackage,
+      protoPath: join(__dirname, 'proto/auth.proto'),
+      url: `0.0.0.0:${configService.grpcPort}`,
+    },
+  });
 
+  app.setGlobalPrefix(globalPrefix);
   app.use(cookieParser());
 
   app.enableCors({
@@ -28,12 +39,15 @@ async function bootstrap() {
     })
   );
 
+  // Start both HTTP and gRPC servers
+  await app.startAllMicroservices();
   const port = configService.port;
   await app.listen(port);
 
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
+  Logger.log(`🚀 gRPC Server is running on: 0.0.0.0:${configService.grpcPort}`);
 }
 
 bootstrap();
